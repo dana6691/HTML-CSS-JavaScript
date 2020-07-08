7526742ab2238c73ea3ad86cd2c1fcf033dac40b
dana6691 <dahee.kim@mnsu.edu>
2020-07-08 13:27:58 -0500

    UPdate

--- Database/Hatchery/Hatchery.js
+++
@@ -1,5 +0,0 @@
-d3.csv('https://raw.githubusercontent.com/dana6691/shinyR/master/micro3.csv?token=AHAYWYELYGRMXYGIMKMNPP26R5WHE',
-  d3.autoType)
-  .then(function (data) {
-    console.log(data)
-  });
